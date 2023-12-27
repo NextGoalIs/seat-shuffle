91cf5c5b58e75e97c415a7d602a7512940077667
NextGoalIs <jinkyo.park@brique.co.kr>
2023-12-27 11:02:43 +0900

    feat: Seats 컴포넌트 레이아웃

---
+++ src/components/Seats.tsx
@@ -0,0 +1,110 @@
+const Seats = () => {
+  return (
+    <div className="text-6xl w-full h-full flex justify-center items-center">
+      <div className="w-[500px] h-[500px] bg-pink-100 flex flex-wrap">
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="1"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="2"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="3"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="4"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="5"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="6"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="7"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="8"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="9"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="10"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="11"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="12"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="13"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="14"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="15"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="16"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="17"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="18"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="19"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="20"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="21"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="22"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="23"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="24"
+        ></div>
+        <div
+          className="flex justify-center items-center w-[100px] h-[100px] bg-slate-400 border-4"
+          id="25"
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Seats;
